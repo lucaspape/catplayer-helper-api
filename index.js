e503767bbd35cac6d18ef0c4f3d40d683c13cc83
Lucas Pape <lucas.pape@web.de>
2019-12-29 23:52:03 +0100

    Check for correct song

--- index.js
+++ index.js
@@ -85,46 +85,66 @@ app.post(APIPREFIX + '/playlist', (req, res) => {
 
 app.delete(APIPREFIX + "/playlist", (req,res) => {
   const playlistId = req.body.playlistId;
-  const songDeleteIndex = req.body.songDeleteIndex;
+  const songDelete = req.body.songDelete;
   const sid = req.body.sid;
 
   if(playlistId !== undefined){
     if(songDeleteIndex !== undefined){
       if(sid !== undefined){
-        request({
-          url: 'https://connect.monstercat.com/v2/playlist/' + playlistId,
-          method: 'GET',
-          headers: {
-            'Cookie': 'connect.sid=' + sid
-          }
-        }, function(err, resp, body){
-          if(err){
-            res.send(err);
+        if(songDelete.songDeleteIndex !== undefined){
+          if(songDelete.releaseId !== undefined){
+            if(songDelete.trackId !== undefined){
+              request({
+                url: 'https://connect.monstercat.com/v2/playlist/' + playlistId,
+                method: 'GET',
+                headers: {
+                  'Cookie': 'connect.sid=' + sid
+                }
+              }, function(err, resp, body){
+                if(err){
+                  res.send(err);
+                }else{
+                  const jsonResponse = JSON.parse(body)
+                  const trackArray = jsonResponse.tracks
+
+                  const patchObject = {};
+
+                  patchObject.tracks = trackArray;
+
+                  if(patchObject.tracks[songDelete.songDeleteIndex].releaseId === songDelete.releaseId){
+                    if(patchObject.tracks[songDelete.songDeleteIndex].trackId === songDelete.trackId){
+                      patchObject.tracks.splice(songDeleteIndex,1);
+                      request({
+                        url: 'https://connect.monstercat.com/v2/playlist/' + playlistId,
+                        method: 'PATCH',
+                        headers: {
+                          'Cookie': 'connect.sid=' + sid
+                        },
+                        json: patchObject
+                      }, function(patchErr, patchResp, patchBody){
+                        if(patchErr){
+                          res.send(patchErr);
+                        }else{
+                          res.json((patchBody));
+                        }
+                      });
+                    }
+                  }else{
+                    res.send("trackId not correct");
+                  }
+                }else{
+                  res.send("releaseId not correct");
+                }
+              });
+            }else{
+              res.send("trackId not specified");
+            }
           }else{
-            const jsonResponse = JSON.parse(body)
-            const trackArray = jsonResponse.tracks
-
-            const patchObject = {};
-
-            patchObject.tracks = trackArray;
-            patchObject.tracks.splice(songDeleteIndex,1);
-
-            request({
-              url: 'https://connect.monstercat.com/v2/playlist/' + playlistId,
-              method: 'PATCH',
-              headers: {
-                'Cookie': 'connect.sid=' + sid
-              },
-              json: patchObject
-            }, function(patchErr, patchResp, patchBody){
-              if(patchErr){
-                res.send(patchErr);
-              }else{
-                res.json((patchBody));
-              }
-            });
+            res.send("releaseId not specified");
           }
-        });
+        }else{
+          res.send("songDeleteIndex not specified");
+        }
       }else{
         res.send("sid not specified");
       }
