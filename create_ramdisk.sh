95a39ef2c0986c8ff226657db60b1f450c6a04f1
Lucas Pape <lucas.pape@web.de>
2020-03-20 20:45:16 +0000

    Ramdisk script

---
+++ create_ramdisk.sh
@@ -0,0 +1,6 @@
+sudo mkdir /media/ramdisk/lucas
+sudo chown lucas /media/ramdisk/lucas
+mkdir /media/ramdisk/lucas/screenshots/
+mkdir /media/ramdisk/lucas/extracted/
+ln -s /media/ramdisk/lucas/screenshots/ screenshots
+ln -s /media/ramdisk/lucas/extracted/ extracted
