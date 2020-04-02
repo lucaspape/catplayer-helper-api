acb82cdea23d282cff96ba946c067735d3dee17c
Lucas Pape <lucas.pape@web.de>
2020-04-02 21:15:57 +0000

    Add custom catalog browse

--- javascript/index.js
+++ javascript/index.js
@@ -11,6 +11,11 @@ const logDB = lowdb(logAdapter);
 logDB.defaults({ requests: []})
   .write();
 
+const catalogDBAdapter = new FileSync('catalog-db.json');
+const catalogDB = lowdb(catalogDBAdapter);
+catalogDB.defaults({ tracks: []})
+  .write();
+
 const PORT = 5000;
 const HOSTNAME = 'http://127.0.0.1:' + PORT;
 const APIPREFIX = '/v1';
@@ -48,6 +53,94 @@ app.get(APIPREFIX + '/liveinfo', (req, res) => {
   res.send(JSON.parse(fs.readFileSync('currentdata.json')));
 });
 
+app.get(APIPREFIX + '/catalog/browse', (req,res) =>{
+  logDB.get('requests')
+    .push({ time: Math.floor(new Date()), url: '/catalog/browse'})
+    .write();
+
+  var skip = parseInt(req.query.skip);
+  var limit = parseInt(req.query.limit);
+
+  if(skip === undefined){
+    skip = 0;
+  }
+
+  if(limit > 50 || limit === undefined){
+    limit = 50;
+  }
+
+  const trackArray = catalogDB.get('tracks').value().reverse()
+  var returnObject = {
+    results : trackArray.slice(skip, skip+limit)
+  };
+
+  res.send((returnObject));
+});
+
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
+
+var updateCatalogDatabase = function(){
+  const removeKeys = ['streamable', 'downloadable', 'inEarlyAccess'];
+
+  request({
+    url: 'https://connect.monstercat.com/v2/catalog/browse?limit=50&skip=' + 0,
+    method: 'GET'
+  }, function(err, resp, body){
+    if(err){
+      console.log(err);
+    }else{
+      var respJson = JSON.parse(body);
+
+      const total = respJson.total;
+
+      var savedTracks = catalogDB.get('tracks').value().length;
+      var rest = ((total - savedTracks) % 50);
+      if(rest === 0){
+        rest = savedTracks;
+      }
+      console.log(rest);
+
+      var skip = total - rest;
+
+      if(skip >= 0){
+          console.log('Skip: ' + skip)
+
+        request({
+          url: 'https://connect.monstercat.com/v2/catalog/browse?limit=50&skip=' + skip,
+          method: 'GET'
+        }, function(err, result, body){
+          if(err){
+            console.log(err);
+          }else{
+            var result = JSON.parse(body).results;
+            result.reverse();
+
+            for(var i=0; i<result.length; i++){
+              var track = result[i];
+
+              for(var k=0; k<removeKeys.length; k++){
+                delete track[removeKeys[k]];
+              }
+
+              catalogDB.get('tracks')
+                .push(track)
+                .write();
+            }
+
+            setTimeout(function(){
+              updateCatalogDatabase();
+            }, 100);
+          }
+        });
+      }else{
+        setTimeout(function(){
+          updateCatalogDatabase();
+        }, 5000);
+      }
+  }
+});
+}
+
+updateCatalogDatabase();
