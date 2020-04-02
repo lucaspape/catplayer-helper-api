d1eab3d47992c12e4bc0395e718c5caa8f952de2
Lucas Pape <lucas.pape@web.de>
2020-04-02 17:32:00 +0000

    Adjust config

--- create_ramdisk.sh
+++ create_ramdisk.sh
@@ -1,5 +1,9 @@
+rm -rf screenshots/
+rm -rf extracted/
+rm -rf javascript/screenshots/
+rm -rf javascript/extracted/
 sudo mkdir /media/ramdisk/lucas
-sudo chown lucas /media/ramdisk/lucas
+sudo chown api /media/ramdisk/lucas
 mkdir /media/ramdisk/lucas/screenshots/
 mkdir /media/ramdisk/lucas/extracted/
 ln -s /media/ramdisk/lucas/screenshots/ screenshots
