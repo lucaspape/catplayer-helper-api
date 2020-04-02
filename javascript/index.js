204c41ce74c489191cae7a2ed65f8a2e94df58db
Lucas Pape <lucas.pape@web.de>
2020-04-02 23:53:34 +0000

    Sort by confidence

--- javascript/index.js
+++ javascript/index.js
@@ -99,23 +99,39 @@ app.get(APIPREFIX + '/catalog/search', (req,res) =>{
   const trackArray = [];
 
   if(titleArray.length > 0){
+    for(var i=0; i<titleArray.length;i++){
+      titleArray[i].confidence = similarity(titleArray[i].title, searchString);
+    }
+
     trackArray.push(...titleArray);
   }
 
   if(versionArray.length > 0){
-      trackArray.push(...versionArray);
+    for(var i=0; i<versionArray.length;i++){
+      versionArray[i].confidence = similarity(versionArray[i].version, searchString);
+    }
+
+    trackArray.push(...versionArray);
   }
 
   if(titleVersionArray.length > 0){
+    for(var i=0; i<titleVersionArray.length;i++){
+      titleVersionArray[i].confidence = similarity(titleVersionArray[i].title + " " + titleVersionArray[i].title, searchString);
+    }
+
     trackArray.push(...titleVersionArray);
   }
 
   if(artistArray.length> 0){
+    for(var i=0; i<artistArray.length;i++){
+      artistArray[i].confidence = similarity(artistArray[i].artistsTitle, searchString);
+    }
+
     trackArray.push(...artistArray);
   }
 
   var returnObject = {
-    results : trackArray
+    results : trackArray.sort((a,b) => (a.confidence - b.confidence));
   };
 
   res.send(returnObject);
@@ -188,3 +204,48 @@ var updateCatalogDatabase = function(){
 }
 
 updateCatalogDatabase();
+
+function similarity(s1, s2) {
+  if(s1 !== undefined && s2 !== undefined){
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return ((longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)) * 100.0;
+  }else{
+    return 0.0;
+  }
+}
+
+function editDistance(s1, s2) {
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  var costs = new Array();
+  for (var i = 0; i <= s1.length; i++) {
+    var lastValue = i;
+    for (var j = 0; j <= s2.length; j++) {
+      if (i == 0)
+        costs[j] = j;
+      else {
+        if (j > 0) {
+          var newValue = costs[j - 1];
+          if (s1.charAt(i - 1) != s2.charAt(j - 1))
+            newValue = Math.min(Math.min(newValue, lastValue),
+              costs[j]) + 1;
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+    }
+    if (i > 0)
+      costs[s2.length] = lastValue;
+  }
+  return costs[s2.length];
+}
