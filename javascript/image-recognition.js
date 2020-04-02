6cb58dcb100bd6eca3b6b5a0e303aacc0d2cfd1c
Lucas Pape <lucas.pape@web.de>
2020-04-02 17:39:24 +0000

    Only printable ascii chars

--- javascript/image-recognition.js
+++ javascript/image-recognition.js
@@ -74,7 +74,7 @@ const tesseractOptions = {
 var recognizeText = function(imagePath, finishedCallback, errorCallback){
   tesseract.recognize(imagePath, tesseractOptions)
     .then(text => {
-      finishedCallback(text.replace('\n\u000c', ''));
+      finishedCallback(text.replace(/[^ -~]+/g, ""));
     }).catch((error) => {
       console.log(error);
       fs.unlinkSync(imagePath);
