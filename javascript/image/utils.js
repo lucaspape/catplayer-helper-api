9f21b8bb88790980d108c7d3944b695c7f1a625b
Lucas Pape <lucas.pape@web.de>
2020-04-06 19:08:57 +0000

    Syntax

--- javascript/image/utils.js
+++ javascript/image/utils.js
@@ -1,3 +1,6 @@
+const request = require('request');
+const fs = require('fs');
+
 module.exports = {
   download: function(uri, filename, callback) {
     request.head(uri, function(err, res, body) {
