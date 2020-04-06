5ea168428631284cb2ca9cdc97fc524444687f7d
Lucas Pape <lucas.pape@web.de>
2020-04-06 18:31:37 +0000

    Cleanup

--- javascript/database/host/database-catalog.js
+++ javascript/database/host/database-catalog.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const utils = require('javascript/database/host/utils.js');
 
 const PORT = 80;
 const HOSTNAME = 'http://127.0.0.1:' + PORT;
@@ -29,46 +30,34 @@ app.get(APIPREFIX + '/catalog', (req, res) => {
   db.defaults(catalogDBDefaults)
     .write();
 
-  var skip = 0;
-  var limit = 50;
-  var gold = false
+  utils.fixSkipAndLimit(req.query, function(skip, limit) {
+    var gold = false;
 
-  if (req.query.skip !== undefined) {
-    skip = parseInt(req.query.skip);
-  }
-
-  if (req.query.limit !== undefined) {
-    limit = parseInt(req.query.limit);
-
-    if (limit > 50) {
-      limit = 50;
+    if (req.query.gold !== undefined) {
+      if (req.query.gold === true) {
+        gold = true;
+      }
     }
-  }
 
-  if (req.query.gold !== undefined) {
-    if (req.query.gold === true) {
-      gold = true;
+    var trackArray = [];
+
+    if (gold) {
+      trackArray = db.get('tracksGold').sortBy('sortId').slice(skip, skip + limit).value();
+    } else {
+      trackArray = db.get('tracks').sortBy('sortId').slice(skip, skip + limit).value();
     }
-  }
 
-  var trackArray = [];
+    for (var i = 0; i < trackArray.length; i++) {
+      delete trackArray[i]['sortId'];
+      delete trackArray[i]['search'];
+    }
 
-  if (gold) {
-    trackArray = db.get('tracksGold').sortBy('sortId').slice(skip, skip + limit).value();
-  } else {
-    trackArray = db.get('tracks').sortBy('sortId').slice(skip, skip + limit).value();
-  }
+    var returnObject = {
+      results: trackArray
+    };
 
-  for (var i = 0; i < trackArray.length; i++) {
-    delete trackArray[i]['sortId'];
-    delete trackArray[i]['search'];
-  }
-
-  var returnObject = {
-    results: trackArray
-  };
-
-  res.send(returnObject);
+    res.send(returnObject);
+  });
 });
 
 app.get(APIPREFIX + '/catalog/search', (req, res) => {
@@ -77,108 +66,51 @@ app.get(APIPREFIX + '/catalog/search', (req, res) => {
   db.defaults(catalogDBDefaults)
     .write();
 
-  var skip = 0;
-  var limit = 50;
-  var gold = false;
+  utils.fixSkipAndLimit(req.query, function(skip, limit) {
+    var gold = false;
 
-  if (req.query.skip !== undefined) {
-    skip = parseInt(req.query.skip);
-  }
-
-  if (req.query.limit !== undefined) {
-    limit = parseInt(req.query.limit);
-
-    if (limit > 50) {
-      limit = 50;
+    if (req.query.gold !== undefined) {
+      if (req.query.gold === true) {
+        gold = true;
+      }
     }
-  }
 
-  if (req.query.gold !== undefined) {
-    if (req.query.gold === true) {
-      gold = true;
+    const searchString = utils.fixSearchString(req.query.term);
+    const terms = searchString.split(' ');
+
+    var trackArray = [];
+
+    if (gold) {
+      trackArray = db.get('tracksGold').filter(track => new RegExp(terms[0], 'i').test(track.search)).value();
+    } else {
+      trackArray = db.get('tracks').filter(track => new RegExp(terms[0], 'i').test(track.search)).value();
     }
-  }
-
-  const searchString = req.query.term.trim();
-  const terms = searchString.replace(/[^\x20-\x7E]/g, "").split(' ');
-
-  var trackArray = [];
-
-  if (gold) {
-    trackArray = db.get('tracksGold').filter(track => new RegExp(terms[0], 'i').test(track.search)).value();
-  } else {
-    trackArray = db.get('tracks').filter(track => new RegExp(terms[0], 'i').test(track.search)).value();
-  }
 
 
-  for (var k = 1; k < terms.length; k++) {
-    trackArray = trackArray.filter(track => new RegExp(terms[k], 'i').test(track.search));
-  }
+    for (var k = 1; k < terms.length; k++) {
+      trackArray = trackArray.filter(track => new RegExp(terms[k], 'i').test(track.search));
+    }
 
-  for (var i = 0; i < trackArray.length; i++) {
-    trackArray[i].similarity = similarity(trackArray[i].search, searchString);
-  }
+    for (var i = 0; i < trackArray.length; i++) {
+      trackArray[i].similarity = utils.similarity(trackArray[i].search, searchString);
+    }
 
-  trackArray = trackArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
+    trackArray = trackArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
 
-  for (var i = 0; i < trackArray.length; i++) {
-    delete trackArray[i]['sortId'];
-    delete trackArray[i]['similarity'];
-    delete trackArray[i]['search'];
-  }
+    for (var i = 0; i < trackArray.length; i++) {
+      delete trackArray[i]['sortId'];
+      delete trackArray[i]['similarity'];
+      delete trackArray[i]['search'];
+    }
 
-  const returnObject = {
-    results: trackArray.slice(skip, skip + limit)
-  }
+    const returnObject = {
+      results: trackArray.slice(skip, skip + limit)
+    }
 
-  res.send(returnObject);
+    res.send(returnObject);
+  });
 });
 
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
-
-function similarity(s1, s2) {
-  if (s1 !== undefined && s2 !== undefined) {
-    var longer = s1;
-    var shorter = s2;
-    if (s1.length < s2.length) {
-      longer = s2;
-      shorter = s1;
-    }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
-      return 1.0;
-    }
-    return ((longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)) * 100.0;
-  } else {
-    return 0.0;
-  }
-}
-
-function editDistance(s1, s2) {
-  s1 = s1.toLowerCase();
-  s2 = s2.toLowerCase();
-
-  var costs = new Array();
-  for (var i = 0; i <= s1.length; i++) {
-    var lastValue = i;
-    for (var j = 0; j <= s2.length; j++) {
-      if (i == 0)
-        costs[j] = j;
-      else {
-        if (j > 0) {
-          var newValue = costs[j - 1];
-          if (s1.charAt(i - 1) != s2.charAt(j - 1))
-            newValue = Math.min(Math.min(newValue, lastValue),
-              costs[j]) + 1;
-          costs[j - 1] = lastValue;
-          lastValue = newValue;
-        }
-      }
-    }
-    if (i > 0)
-      costs[s2.length] = lastValue;
-  }
-  return costs[s2.length];
-}
--- javascript/database/host/database-releases.js
+++ javascript/database/host/database-releases.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const utils = require('javascript/database/host/utils.js');
 
 const PORT = 80;
 const HOSTNAME = 'http://127.0.0.1:' + PORT;
@@ -29,46 +30,27 @@ app.get(APIPREFIX + '/releases', (req, res) => {
   db.defaults(releasesDBDefaults)
     .write();
 
-  var skip = 0;
-  var limit = 50;
-  var gold = false;
+  utils.fixSkipAndLimit(req.query, function(skip, limit) {
+    var gold = false;
+    var releaseArray = [];
 
-  if (req.query.skip !== undefined) {
-    skip = parseInt(req.query.skip);
-  }
-
-  if (req.query.limit !== undefined) {
-    limit = parseInt(req.query.limit);
-
-    if (limit > 50) {
-      limit = 50;
+    if (gold) {
+      releaseArray = db.get('releasesGold').sortBy('sortId').slice(skip, skip + limit).value();
+    } else {
+      releaseArray = db.get('releases').sortBy('sortId').slice(skip, skip + limit).value();
     }
-  }
 
-  if (req.query.gold !== undefined) {
-    if (req.query.gold === true) {
-      gold = true;
+    for (var i = 0; i < releaseArray.length; i++) {
+      delete releaseArray[i]['sortId'];
+      delete releaseArray[i]['search'];
     }
-  }
 
-  var releaseArray = [];
+    var returnObject = {
+      results: releaseArray
+    };
 
-  if (gold) {
-    releaseArray = db.get('releasesGold').sortBy('sortId').slice(skip, skip + limit).value();
-  } else {
-    releaseArray = db.get('releases').sortBy('sortId').slice(skip, skip + limit).value();
-  }
-
-  for (var i = 0; i < releaseArray.length; i++) {
-    delete releaseArray[i]['sortId'];
-    delete releaseArray[i]['search'];
-  }
-
-  var returnObject = {
-    results: releaseArray
-  };
-
-  res.send(returnObject);
+    res.send(returnObject);
+  });
 });
 
 app.get(APIPREFIX + '/releases/search', (req, res) => {
@@ -77,107 +59,43 @@ app.get(APIPREFIX + '/releases/search', (req, res) => {
   db.defaults(releasesDBDefaults)
     .write();
 
-  var skip = 0;
-  var limit = 50;
-  var gold = false;
+  utils.fixSkipAndLimit(req.query, function(skip, limit) {
+    var gold = false;
+    const searchString = utils.fixSearchString(req.query.term);
+    const terms = searchString.split(' ');
 
-  if (req.query.skip !== undefined) {
-    skip = parseInt(req.query.skip);
-  }
+    var releaseArray = [];
 
-  if (req.query.limit !== undefined) {
-    limit = parseInt(req.query.limit);
-
-    if (limit > 50) {
-      limit = 50;
+    if (gold) {
+      releaseArray = db.get('releasesGold').filter(release => new RegExp(terms[0], 'i').test(release.search)).value();
+    } else {
+      releaseArray = db.get('releases').filter(release => new RegExp(terms[0], 'i').test(release.search)).value();
     }
-  }
 
-  if (req.query.gold !== undefined) {
-    if (req.query.gold === true) {
-      gold = true;
+    for (var k = 1; k < terms.length; k++) {
+      releaseArray = releaseArray.filter(release => new RegExp(terms[k], 'i').test(release.search));
     }
-  }
 
-  const searchString = req.query.term.trim();
-  const terms = searchString.replace(/[^\x20-\x7E]/g, "").split(' ');
+    for (var i = 0; i < releaseArray.length; i++) {
+      releaseArray[i].similarity = utils.similarity(releaseArray[i].search, searchString);
+    }
 
-  var releaseArray = [];
+    releaseArray = releaseArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
 
-  if (gold) {
-    releaseArray = db.get('releasesGold').filter(release => new RegExp(terms[0], 'i').test(release.search)).value();
-  } else {
-    releaseArray = db.get('releases').filter(release => new RegExp(terms[0], 'i').test(release.search)).value();
-  }
+    for (var i = 0; i < releaseArray.length; i++) {
+      delete releaseArray[i]['sortId'];
+      delete releaseArray[i]['similarity'];
+      delete releaseArray[i]['search'];
+    }
 
-  for (var k = 1; k < terms.length; k++) {
-    releaseArray = releaseArray.filter(release => new RegExp(terms[k], 'i').test(release.search));
-  }
+    const returnObject = {
+      results: releaseArray.slice(skip, skip + limit)
+    }
 
-  for (var i = 0; i < releaseArray.length; i++) {
-    releaseArray[i].similarity = similarity(releaseArray[i].search, searchString);
-  }
-
-  releaseArray = releaseArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
-
-  for (var i = 0; i < releaseArray.length; i++) {
-    delete releaseArray[i]['sortId'];
-    delete releaseArray[i]['similarity'];
-    delete releaseArray[i]['search'];
-  }
-
-  const returnObject = {
-    results: releaseArray.slice(skip, skip + limit)
-  }
-
-  res.send(returnObject);
+    res.send(returnObject);
+  });
 });
 
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
-
-function similarity(s1, s2) {
-  if (s1 !== undefined && s2 !== undefined) {
-    var longer = s1;
-    var shorter = s2;
-    if (s1.length < s2.length) {
-      longer = s2;
-      shorter = s1;
-    }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
-      return 1.0;
-    }
-    return ((longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)) * 100.0;
-  } else {
-    return 0.0;
-  }
-}
-
-function editDistance(s1, s2) {
-  s1 = s1.toLowerCase();
-  s2 = s2.toLowerCase();
-
-  var costs = new Array();
-  for (var i = 0; i <= s1.length; i++) {
-    var lastValue = i;
-    for (var j = 0; j <= s2.length; j++) {
-      if (i == 0)
-        costs[j] = j;
-      else {
-        if (j > 0) {
-          var newValue = costs[j - 1];
-          if (s1.charAt(i - 1) != s2.charAt(j - 1))
-            newValue = Math.min(Math.min(newValue, lastValue),
-              costs[j]) + 1;
-          costs[j - 1] = lastValue;
-          lastValue = newValue;
-        }
-      }
-    }
-    if (i > 0)
-      costs[s2.length] = lastValue;
-  }
-  return costs[s2.length];
-}
