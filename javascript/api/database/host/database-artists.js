695444d9b12dd36f50fc78ffcfe63eb3a9e994ff
Lucas Pape <lucas.pape@web.de>
2020-04-07 16:16:30 +0000

    Search artist with sql

--- javascript/api/database/host/database-artists.js
+++ javascript/api/database/host/database-artists.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const lowdb = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const utils = require('./utils.js');
 
 const PORT = 80;
 const HOSTNAME = 'http://127.0.0.1:' + PORT;
 const APIPREFIX = '';
-
-const artistsDBFile = 'db-artists.json';
-
-const artistsDBDefaults = {
-  artists: []
-}
 
 const app = express();
 app.use(cors());
@@ -78,38 +68,35 @@ createDatabaseConnection.connect(err => {
             });
 
             app.get(APIPREFIX + '/artists/search', (req, res) => {
-              const dbAdapter = new FileSync(artistsDBFile);
-              const db = lowdb(dbAdapter);
-              db.defaults(artistsDBDefaults)
-                .write();
-
               utils.fixSkipAndLimit(req.query, function(skip, limit) {
-                const searchString = utils.fixSearchString(req.query.term)
+                onst searchString = utils.fixSearchString(req.query.term)
                 const terms = searchString.split(' ');
 
-                var artistsArray = db.get('artists').filter(release => new RegExp(terms[0], 'i').test(release.search)).value();
+                const artistsSearchQuery = 'SELECT * FROM `' + dbName + '`.`artists` ORDER BY sortId ASC LIMIT ' + skip + ', ' + limit + ' WHERE search LIKE "%' + terms[0] + '%";';
 
-                for (var k = 1; k < terms.length; k++) {
-                  artistsArray = artistsArray.filter(artist => new RegExp(terms[k], 'i').test(artist.search));
-                }
+                mysqlConnection.query(artistsSearchQuery, (err, result) => {
+                  if (err) {
+                    res.send(err);
+                  } else {
+                    var artistsArray = JSON.parse(results);
 
-                for (var i = 0; i < artistsArray.length; i++) {
-                  artistsArray[i].similarity = utils.similarity(artistsArray[i].search, searchString);
-                }
+                    for (var k = 1; k < terms.length; k++) {
+                      artistsArray = artistsArray.filter(artist => new RegExp(terms[k], 'i').test(artist.search));
+                    }
 
-                artistsArray = artistsArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
+                    for (var i = 0; i < artistsArray.length; i++) {
+                      artistsArray[i].similarity = utils.similarity(artistsArray[i].search, searchString);
+                    }
 
-                for (var i = 0; i < artistsArray.length; i++) {
-                  delete artistsArray[i]['sortId'];
-                  delete artistsArray[i]['similarity'];
-                  delete artistsArray[i]['search'];
-                }
+                    artistsArray = artistsArray.sort((a, b) => (a.similarity - b.similarity)).reverse();
 
-                const returnObject = {
-                  results: artistsArray.slice(skip, skip + limit)
-                }
+                    const returnObject = {
+                      results: artistsArray.slice(skip, skip + limit)
+                    }
 
-                res.send(returnObject);
+                    res.send(returnObject);
+                  }
+                });
               });
             });
 
