5f9727e16155ee546b4eca5f7223e7eef7fb83d3
Lucas Pape <lucas.pape@web.de>
2020-04-14 16:06:31 +0000

    Add skip and limit

--- javascript/api/api/related-api.js
+++ javascript/api/api/related-api.js
@@ -24,99 +24,97 @@ const mysqlConnection = mysql.createConnection({
 });
 
 mysqlConnection.connect(err => {
-  if (err) {
-    console.log(err);
-    return err;
-  } else {
-    app.post(APIPREFIX + '/', (req, res) => {
-      console.log(req.body);
-
-      const tracks = req.body;
-
-      getSearchFromIds(tracks, mysqlConnection, function(search) {
-        var catalogSongQuery = 'SELECT id,search FROM `' + dbName + '`.`catalog` WHERE ' + 'id!="' + search[0].id + '" ';
-
-        for (var i = 1; i < search.length; i++) {
-          catalogSongQuery += 'AND id != "' + search[i].id + '" ';
-        }
-
-        catalogSongQuery += ';';
-
-        console.log(catalogSongQuery);
-
-        mysqlConnection.query(catalogSongQuery, (err, result) => {
-          if (err) {
-            res.send(err);
-          } else {
-            var arrayWithSimiliarity = [];
-
-            for (var i = 0; i < search.length; i++) {
-              //remove id from search
-              var firstSearch = search[i].search.replace(search[i].id, '');
-
-              for (var k = 0; k < result.length; k++) {
-                //remove id from search
-                var secondSearch = result[k].search.replace(result[k].id, '');
-                var similarity = utils.similarity(firstSearch, secondSearch);
-
-                if (arrayWithSimiliarity[k] !== undefined) {
-                  similarity += arrayWithSimiliarity[k].similarity;
-                }
-
-                arrayWithSimiliarity[k] = {
-                  id: result[k].id,
-                  similarity: similarity
-                };
-              }
-            }
-
-            //sort
-            arrayWithSimiliarity.sort(function(a, b) {
-              if (a.similarity < b.similarity) return 1;
-              if (a.similarity > b.similarity) return -1;
-              return 0;
-            });
-
-            res.send({
-              results: arrayWithSimiliarity
-            });
-          }
-        });
-      }, function(err) {
+      if (err) {
         console.log(err);
-        res.send(err);
-      })
-    });
+        return err;
+      } else {
+        app.post(APIPREFIX + '/', (req, res) => {
+            utils.fixSkipAndLimit(req.query, function(skip, limit) {
+                const tracks = req.body;
 
-    app.listen(PORT, () => {
-      console.log('Server started on port ' + PORT);
-    });
-  }
-});
+                getSearchFromIds(tracks, mysqlConnection, function(search) {
+                  var catalogSongQuery = 'SELECT id,search FROM `' + dbName + '`.`catalog` WHERE ' + 'id!="' + search[0].id + '" ';
 
-function getSearchFromIds(trackArray, mysqlConnection, callback, errorCallback) {
-  var trackSearch = [];
+                  for (var i = 1; i < search.length; i++) {
+                    catalogSongQuery += 'AND id != "' + search[i].id + '" ';
+                  }
 
-  var i = 0;
+                  catalogSongQuery += ';';
 
-  var sqlCallback = function() {
-    if (i < trackArray.length) {
-      const catalogSongQuery = 'SELECT id,search FROM `' + dbName + '`.`catalog` WHERE id="' + trackArray[i].id + '";';
+                  mysqlConnection.query(catalogSongQuery, (err, result) => {
+                    if (err) {
+                      res.send(err);
+                    } else {
+                      var arrayWithSimiliarity = [];
 
-      mysqlConnection.query(catalogSongQuery, (err, result) => {
-        if (err) {
-          errorCallback(err);
-        } else {
-          trackSearch[i] = result[0];
-          i++;
-          sqlCallback();
+                      for (var i = 0; i < search.length; i++) {
+                        //remove id from search
+                        var firstSearch = search[i].search.replace(search[i].id, '');
+
+                        for (var k = 0; k < result.length; k++) {
+                          //remove id from search
+                          var secondSearch = result[k].search.replace(result[k].id, '');
+                          var similarity = utils.similarity(firstSearch, secondSearch);
+
+                          if (arrayWithSimiliarity[k] !== undefined) {
+                            similarity += arrayWithSimiliarity[k].similarity;
+                          }
+
+                          arrayWithSimiliarity[k] = {
+                            id: result[k].id,
+                            similarity: similarity
+                          };
+                        }
+                      }
+
+                      //sort
+                      arrayWithSimiliarity.sort(function(a, b) {
+                        if (a.similarity < b.similarity) return 1;
+                        if (a.similarity > b.similarity) return -1;
+                        return 0;
+                      });
+
+                      res.send({
+                        results: arrayWithSimiliarity.slice(skip, skip + limit)
+                      });
+                    }
+                  });
+                }, function(err) {
+                  console.log(err);
+                  res.send(err);
+                })
+              }
+            });
+
+          app.listen(PORT, () => {
+            console.log('Server started on port ' + PORT);
+          });
         }
       });
-    } else {
-      //DONE
-      callback(trackSearch);
-    }
-  }
 
-  sqlCallback();
-}
+    function getSearchFromIds(trackArray, mysqlConnection, callback, errorCallback) {
+      var trackSearch = [];
+
+      var i = 0;
+
+      var sqlCallback = function() {
+        if (i < trackArray.length) {
+          const catalogSongQuery = 'SELECT id,search FROM `' + dbName + '`.`catalog` WHERE id="' + trackArray[i].id + '";';
+
+          mysqlConnection.query(catalogSongQuery, (err, result) => {
+            if (err) {
+              errorCallback(err);
+            } else {
+              trackSearch[i] = result[0];
+              i++;
+              sqlCallback();
+            }
+          });
+        } else {
+          //DONE
+          callback(trackSearch);
+        }
+      }
+
+      sqlCallback();
+    }
