7d43ef70afdca578ee2b4545fec2f3e4e7c1df4e
Lucas Pape <lucas.pape@web.de>
2020-04-16 17:29:07 +0000

    Scale

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 docker-compose down
 docker-compose down -v
 git pull
-docker-compose up --force-recreate --build -d
+docker-compose up --force-recreate --build -d --scale web=5 --scale related-api=5
