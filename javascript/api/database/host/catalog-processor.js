11f755381e5c0be1704315d79e4ddb552df1d94b
Lucas Pape <lucas.pape@web.de>
2020-05-02 23:30:56 +0000

    Connect in function

--- javascript/api/database/host/catalog-processor.js
+++ javascript/api/database/host/catalog-processor.js
@@ -10,34 +10,34 @@ const mysqlConnection = mysql.createConnection({
     database: dbName
 });
 
-mysqlConnection.connect(err => {
-    if (err) {
-        console.log(err);
-        return err;
-    } else {
-        function processCatalogSearch(terms, trackArray, skip, limit, gold, callback, errorCallback) {
+function processCatalogSearch(terms, trackArray, skip, limit, gold, callback, errorCallback) {
+    mysqlConnection.connect(err => {
+        if (err) {
+            console.log(err);
+            errorCallback(err);
+        } else {
             for (var k = 1; k < terms.length; k++) {
                 trackArray = trackArray.filter(track => new RegExp(terms[k], 'i').test(track.search));
             }
-
+        
             for (var i = 0; i < trackArray.length; i++) {
                 trackArray[i].similarity = utils.similarity(trackArray[i].search.replace(trackArray[i].id, ''), searchString);
             }
-
+        
             trackArray.sort(function (a, b) {
                 if (a.similarity < b.similarity) return 1;
                 if (a.similarity > b.similarity) return -1;
                 return 0;
             });
-
+        
             trackArray = trackArray.slice(skip, skip + limit);
-
+        
             var i = 0;
-
+        
             var releasesQueryFinished = function () {
                 if (i < trackArray.length) {
                     const releaseQuery = 'SELECT artistsTitle, catalogId, id, releaseDate, title, type FROM `' + dbName + '`.`releases` WHERE id="' + trackArray[i].releaseId + '";';
-
+        
                     mysqlConnection.query(releaseQuery, (err, releaseResult) => {
                         if (err) {
                             errorCallback(err);
@@ -55,14 +55,14 @@ mysqlConnection.connect(err => {
                     callback(trackArray);
                 }
             };
-
+        
             releasesQueryFinished();
         }
+    });
+}
 
-        process.on('message', async (data) => {
-            processCatalogSearch(data.terms, data.trackArray, data.skip, data.limit, data.gold, function (result) {
-                process.send({ result: result });
-            }, function (err) { process.send({ err: err }) });
-        });
-    }
+process.on('message', async (data) => {
+    processCatalogSearch(data.terms, data.trackArray, data.skip, data.limit, data.gold, function (result) {
+        process.send({ result: result });
+    }, function (err) { process.send({ err: err }) });
 });
