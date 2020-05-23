e2ae62c76851c94271f641f21dbdf746e6520089
Lucas Pape <lucas.pape@web.de>
2020-05-23 16:14:54 +0000

    Simple newsletter notification

--- notifications/news-notifications.js
+++ notifications/news-notifications.js
@@ -1,26 +1,77 @@
 var admin = require("firebase-admin");
+var imap = require("imap-simple");
+var HTMLParser = require('node-html-parser');
 
-var serviceAccount = require("./firebase/config.json");
+var serviceAccount = require("./config/firebase_config.json");
+var mailConfig = require("./config/mail_config.json")
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://player-for-monsterdcat.firebaseio.com"
-});
+})
 
-var message = {
-  data: {
-   title: 'mctv',
-   msg: 'This week on mctv... Monstercat Instinct takes over with Duumu, SMLE, and CloudNone on Thurs May 28th. Tune in for dj sets and Q&As!',
-   url: 'https://live.monstercat.com/'
-  },
-  topic: 'default'
-};
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-admin.messaging().send(message)
-.then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
+var oldSubjects = [];
+
+var loop = function(){
+  return imap.connect(mailConfig).then(function(connection){
+    return connection.openBox('INBOX').then(function () {
+      var searchCriteria = [
+          'UNSEEN',
+         [ 'FROM', 'info@monstercat.com']
+      ];
+
+      var fetchOptions = {
+          bodies: ['HEADER', 'TEXT'],
+          markSeen: true
+      };
+
+      return connection.search(searchCriteria, fetchOptions).then(function (results) {
+          var bodys = results.map(function(res){
+            return res.parts.filter(function(part){
+              return part.which === 'TEXT';
+            })[0].body;
+          });
+
+          var subjects = results.map(function (res) {
+              return res.parts.filter(function (part) {
+                  return part.which === 'HEADER';
+              })[0].body.subject[0];
+          });
+
+          if(oldSubjects.length !== subjects.length){
+            oldSubjects = subjects;
+
+            var body = (bodys[bodys.length-1]);
+
+            var root = HTMLParser.parse(body);
+            console.log(root);
+
+            var message = {
+              data: {
+               title: 'Monstercat Newsletter',
+               msg: subjects[subjects.length-1]
+              },
+              topic: 'newsletter'
+            };
+
+            admin.messaging().send(message)
+          .then((response) => {
+              // Response is a message ID string.
+              console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+              console.log('Error sending message:', error);
+            });
+          }
+
+          connection.end();
+
+          loop();
+      });
   });
+});
+}
+
+ loop();
