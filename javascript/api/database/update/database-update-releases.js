8ef34c3c78cd5a9e8530639751b2b908e645f852
Lucas Pape <lucaspape@Lucas-Papes-MacBook-Pro.local>
2020-08-06 18:13:20 +0200

    Remove stuff (to be independent of MC api)

--- javascript/api/database/update/database-update-releases.js
+++ javascript/api/database/update/database-update-releases.js
@@ -1,6 +1,8 @@
 const request = require('request');
-
 const mysql = require('mysql');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const dbName = 'monstercatDB';
 
@@ -9,6 +11,16 @@ const createDatabaseConnection = mysql.createConnection({
   user: 'root',
   password: 'JacPV7QZ'
 });
+
+const PORT = 80;
+const APIPREFIX = '';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 createDatabaseConnection.connect(err => {
   if (err) {
@@ -45,47 +57,18 @@ function initializeDatabase(mysqlConnection) {
     } else {
       console.log('Created releases table');
 
-      initReleases(mysqlConnection, function () {
-        console.log('Done!');
+      app.post(APIPREFIX + '/release', (req, res) => {
+        addToDB(req.body, mysqlConnection, function(){
+          res.send('OK');
+        });
+      });
 
-        //wait 5 minutes
-        setTimeout(function () {
-          initializeDatabase(mysqlConnection);
-        }, 300000);
+      app.listen(PORT, () => {
+        console.log('Server started on port ' + PORT);
+      });
       });
     }
   });
-}
-
-function initReleases(mysqlConnection, callback) {
-  browseReleases(-1, 0,
-    function (json) {
-      console.log('Received release data...');
-      const releasesArray = json.results.reverse();
-
-      var i = 0;
-
-      var sqlCallback = function () {
-        if (i < releasesArray.length) {
-          if (i % 100 === 0) {
-            console.log((i / releasesArray.length) * 100 + '%');
-          }
-
-          addToDB(releasesArray[i], mysqlConnection, function () {
-            i++;
-            sqlCallback();
-          });
-        } else {
-          callback();
-        }
-      };
-
-      sqlCallback();
-    },
-
-    function (err) {
-      console.log(err);
-    });
 }
 
 function addToDB(release, mysqlConnection, callback) {
@@ -119,18 +102,4 @@ function addToDB(release, mysqlConnection, callback) {
 
     callback();
   });
-}
-
-function browseReleases(limit, skip, callback, errorCallback) {
-  request({
-    url: 'https://connect.monstercat.com/v2/releases?limit=' + limit + '&skip=' + skip,
-    method: 'GET'
-  },
-    function (err, resp, body) {
-      if (err) {
-        errorCallback(err);
-      } else {
-        callback(JSON.parse(body));
-      }
-    });
 }
---
+++ javascript/apiclient/apiclient.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const args = require('minimist')(process.argv.slice(2));
+
+switch(args.a){
+  case 'catalog':
+    if(args.json !== undefined){
+      addToCatalog(args.json);
+    }else{
+        console.log('json not defined');
+    }
+
+    break;
+  default:
+    console.log('unknown command');
+}
+
+function addToCatalog(json){
+  request({
+    url: 'http://proxy-internal/database-update-catalog',
+    method: 'POST',
+    json: true,
+    body: json
+  }, function(err, resp, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      try {
+        console.log(body);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+}
