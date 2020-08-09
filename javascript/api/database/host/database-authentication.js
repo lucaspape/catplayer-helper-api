b70e57afa19908e08be0bbda6550e345716b8a66
Lucas Pape <lucaspape@Lucas-Papes-MacBook-Pro.local>
2020-08-09 15:49:30 +0200

    Log

--- javascript/api/database/host/database-authentication.js
+++ javascript/api/database/host/database-authentication.js
@@ -64,7 +64,7 @@ sqlhelper.getConnection(
                   }
                 });
               }else{
-                console.log('DB password: ' + result.password);
+                console.log('DB password: ' + results);
                 console.log('Received password: ' + password)
                 console.log('Wrong password!');
                 res.status(401).send('Wrong password');
