f5748a36dc11312f829c658c8c5c88c5c965cd8d
Lucas Pape <lucas.pape@web.de>
2020-08-28 13:39:49 +0000

    Change distance algorithm

--- native-c/similarity-calculator/calc.c
+++ native-c/similarity-calculator/calc.c
@@ -17,33 +17,58 @@ char * charToLower(const char *s){
   return lower;
 }
 
-int editDistance(const char * i1, const char * i2){
-  char * s1 = charToLower(i1);
-  char * s2 = charToLower(i2);
+int editDistance (const char * word1,
 
-  int costs[strlen(s2)];
+                     const char * word2
+                    )
+{
+  char * s1 = charToLower(word1);
+  char * s2 = charToLower(word2);
 
-  for(int i=0; i<= strlen(s1); i++){
-    int lastValue = i;
+  int len1 = strlen(s1);
+  int len2 = strlen(s2);
 
-    for(int j=0; j<= strlen(s2); j++){
-      if(i==0)
-        costs[j] = j;
-      else {
-        if(j > 0){
-          int newValue = costs[j - 1];
-          if(s1[i-1] != s2[j-1])
-            newValue = MIN(MIN(newValue, lastValue), costs[j]) + 1;
-          costs[j - 1] = lastValue;
-          lastValue = newValue;
-        }
-      }
+    int matrix[len1 + 1][len2 + 1];
+    int i;
+    for (i = 0; i <= len1; i++) {
+        matrix[i][0] = i;
     }
-    if(i > 0)
-      costs[strlen(s2)] = lastValue;
-  }
+    for (i = 0; i <= len2; i++) {
+        matrix[0][i] = i;
+    }
+    for (i = 1; i <= len1; i++) {
+        int j;
+        char c1;
 
-  return costs[strlen(s2)];
+        c1 = word1[i-1];
+        for (j = 1; j <= len2; j++) {
+            char c2;
+
+            c2 = word2[j-1];
+            if (c1 == c2) {
+                matrix[i][j] = matrix[i-1][j-1];
+            }
+            else {
+                int delete;
+                int insert;
+                int substitute;
+                int minimum;
+
+                delete = matrix[i-1][j] + 1;
+                insert = matrix[i][j-1] + 1;
+                substitute = matrix[i-1][j-1] + 1;
+                minimum = delete;
+                if (insert < minimum) {
+                    minimum = insert;
+                }
+                if (substitute < minimum) {
+                    minimum = substitute;
+                }
+                matrix[i][j] = minimum;
+            }
+        }
+    }
+    return matrix[len1][len2];
 }
 
 double similarity(char * s1, char * s2){
