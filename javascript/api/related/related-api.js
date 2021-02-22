e99de05e92b7854af218aa7bbce4eef4e6ba85b4
Lucas Pape <lucas.pape@web.de>
2021-02-22 21:59:10 +0100

    Syntax

--- javascript/api/related/related-api.js
+++ javascript/api/related/related-api.js
@@ -62,7 +62,7 @@ sqlhelper.getConnection(
             var out = stdout.split(/\r?\n/);
             var array = [];
 
-            const catalogQuery = 'SELECT id,artists,artistsTitle,bpm ,creatorFriendly,debutDate,debutTime,duration,explicit,genrePrimary,genreSecondary,isrc,playlistSort,releaseId,tags,title,trackNumber,version,inEarlyAccess,search FROM `' + dbName + '`.`catalog` WHERE id="' + out[0] + '" ';
+            var catalogQuery = 'SELECT id,artists,artistsTitle,bpm ,creatorFriendly,debutDate,debutTime,duration,explicit,genrePrimary,genreSecondary,isrc,playlistSort,releaseId,tags,title,trackNumber,version,inEarlyAccess,search FROM `' + dbName + '`.`catalog` WHERE id="' + out[0] + '" ';
 
             for(var i=1; i<out.length; i++){
               catalogQuery += 'OR id="' + out[i] + '" ';
