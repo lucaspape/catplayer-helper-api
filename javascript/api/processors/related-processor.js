c38b624a1b6d3abc9456d4ba2e1f959fe9815a53
Lucas Pape <lucas.pape@web.de>
2021-03-07 22:51:16 +0100

    Syntax

--- javascript/api/processors/related-processor.js
+++ javascript/api/processors/related-processor.js
@@ -3,8 +3,6 @@ const utils = require('/app/utils.js');
 function processRelated(searchArray, allSongs, gold, callback, errorCallback) {
   utils.sqlhelper.getConnection(
       function (mysqlConnection) {
-        var arrayWithSimiliarity = [];
-
         for (var i = 0; i < searchArray.length; i++) {
             const firstSearch = searchArray[i].search.replace(searchArray[i].id, '');
 
@@ -14,30 +12,27 @@ function processRelated(searchArray, allSongs, gold, callback, errorCallback) {
                 var similarity = utils.similarity(firstSearch, secondSearch);
                 const id = allSongs[k].id;
 
-                if (arrayWithSimiliarity[k] !== undefined) {
-                    similarity += arrayWithSimiliarity[k].similarity;
+                if (allSongs[k].similarity) {
+                    similarity += allSongs[k].similarity;
                 }
 
-                arrayWithSimiliarity[k] = {
-                    id: id,
-                    similarity: similarity
-                };
+                allSongs[k].similarity = similarity;
             }
         }
 
-        arrayWithSimiliarity.sort(function (a, b) {
+        allSongs.sort(function (a, b) {
             if (a.similarity < b.similarity) return 1;
             if (a.similarity > b.similarity) return -1;
             return 0;
         });
 
-        arrayWithSimiliarity = arrayWithSimiliarity.slice(0, 50);
+        var songs = allSongs.slice(0, 50);
 
         var releasesQueryFinished = function () {
-          if (i < arrayWithSimiliarity.length) {
-            utils.getRelease(mysqlConnection, arrayWithSimiliarity[i].releaseId, (release)=>{
-              utils.addMissingTrackKeys(arrayWithSimiliarity[i], gold, release, mysqlConnection, function (track) {
-                arrayWithSimiliarity[i] = track;
+          if (i < songs.length) {
+            utils.getRelease(mysqlConnection, songs[i].releaseId, (release)=>{
+              utils.addMissingTrackKeys(songs[i], gold, release, mysqlConnection, function (track) {
+                songs[i] = track;
                 i++;
                 releasesQueryFinished();
               }, function (err) {
@@ -47,7 +42,7 @@ function processRelated(searchArray, allSongs, gold, callback, errorCallback) {
               res.send(err);
             })
           } else {
-             callback(arrayWithSimiliarity);
+             callback(songs);
           }
         };
 
